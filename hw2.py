f751022d1476c74af7362aba2ca4879a728cceba
vitforlinuxex <vitforlinux.extra@gmail.com>
2025-10-10 18:01:11 +0200

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,24 @@
+print('Hello word')
+
+pippo = input()
+
+print(pippo)
+
+numero = int(input("inserisci "))
+
+if numero == 0:
+    print("Ciao!")
+else: print ("dovevi mettere zero!")
+
+if numero%2 == 0:
+    print("PaRI")
+else: print ("dISPARI")
+
+nome= input("come ti chiami?\n ")
+print(f"ciao {nome}!")
+
+num1 = int(input("inserisci primo numero \n"))
+num2 = int(input("inserisci secondo numero \n"))
+
+somma = num1 + num2
+print(f"la somme di {num1} + {num2} è {num1 + num2}")

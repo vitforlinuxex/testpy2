5e28a559ff97eadf1e6303e17cf07cd9581ee9a0
vitforlinuxex <vitforlinux.extra@gmail.com>
2025-10-29 14:45:23 +0000

    Add files via upload

---
+++ funzione_somma2.py
@@ -0,0 +1,9 @@
+def somma(num1,num2):
+    print("inserisci due numeri farò la somma")
+    #num1 = int(input("primo num "))
+    #num2 = int(input("secondo num "))
+    print(f"somma {num1} e {num2} uguale {num1+num2}")
+    
+num1 = int(input("primo num "))
+num2 = int(input("secondo num "))
+somma(num1,num2)

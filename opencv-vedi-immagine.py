5e6d6c2d9f34eee06f6275de71157cb7ae93fa65
vitforlinuxex <237251197+vitforlinuxex@users.noreply.github.com>
2025-11-21 09:56:53 +0000

    Add files via upload

--- opencv-vedi-immagine.py
+++ opencv-vedi-immagine.py
@@ -1,15 +1,22 @@
 import cv2
-
+while True:
 # Carica l'immagine dal file
 #C:\Users\Studente\Downloads\steampunk-starshipAI5.jpg
-immagine = cv2.imread('C:/Users/Studente/Downloads/steampunk-starshipAI5.jpg')
+    immagine = cv2.imread('C:/Users/Studente/Downloads/steampunk-starshipAI5.jpg')
 
 # Controlla che l'immagine sia stata caricata correttamente
-if immagine is None:
-    print("Errore: impossibile caricare l'immagine.")
-else:
+    if immagine is None:
+        print("Errore: impossibile caricare l'immagine.")
+    else:
     # Mostra l'immagine in una finestra
-    cv2.imshow('Immagine', immagine)
+        cv2.imshow('Immagine', immagine)
 
     # Aspetta la pressione di un tasto per chiudere la finestra
-    cv2.waitKey(0)
+        tasto = cv2.waitKey(1)
+        if tasto==ord("q"):
+            break
+        elif tasto==ord("a"):
+            cv2.imwrite("foto.jpg",immagine)
+            
+cv2.destroyAllWindows()
+    
